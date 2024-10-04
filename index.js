7caec305e9b1bcf660b2e92a728e4b90da7e9e25
Tristan Tritch <ttmeep101@hotmail.com>
2024-10-03 19:09:21 -0500

    completes tasks

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft(){
+    const leftNums = dodger.style.left.replace("px","");
+    const left = parseInt(leftNums, 10);
+
+    if(left > 0){
+        dodger.style.left= `${left-1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftNums = dodger.style.left.replace("px","");
+    const left = parseInt(leftNums, 10);
+
+    if(left > 0){
+        dodger.style.left= `${left+1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e){
+    if (e.key == "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown", function (e){
+    if (e.key == "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
